38f967576fd6f8c108f526a074a66052578962b2
kinou-p <alexpomms@gmail.com>
2023-05-08 17:48:35 +0200

    fix await error front

--- frontend/src/script/axiosApi.js
+++ frontend/src/script/axiosApi.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // const token = localStorage.getItem('token');
 
 
-async function getToken() {
+function getToken() {
 	// your code to retrieve the token from localStorage or any other source
 	const token = localStorage.getItem('token');
 	return token;
@@ -15,7 +15,7 @@ console.log(`getToken = ${getToken()}`)
 const api = axios.create({
   baseURL: 'http://localhost/api',
   headers: {
-	  Authorization: `Bearer ${await getToken()}`,
+	  Authorization: `Bearer ${getToken()}`,
 	},
   withCredentials: true,
 });
