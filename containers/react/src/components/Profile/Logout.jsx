25b955030beea2dcd02107699312d76b88c04345
kinou-p <alexpomms@gmail.com>
2023-06-15 18:17:04 +0200

    auto push

--- containers/react/src/components/Profile/Logout.jsx
+++ containers/react/src/components/Profile/Logout.jsx
@@ -4,7 +4,7 @@
 function Logout(){
 
 	localStorage.clear();
-	const path = 'http://' + process.env.BASE_URL + '/'; 
+	const path = 'http://' + process.env.REACT_APP_BASE_URL + '/'; 
 	// history(path, { replace: true });
 	// window.location.replace(path);
 	// window.history.pushState({}, '', path);
--- containers/react/src/script/axiosApi.js
+++ containers/react/src/script/axiosApi.js
@@ -13,12 +13,12 @@ console.log(`getToken = ${getToken()}`)
 console.log(`Bearer ${localStorage.getItem("token")}`)
 
 const test = "192.168.1.19"
-// const url = 'http://' + process.env.BASE_URL + '/api'
+// const url = 'http://' + process.env.REACT_APP_BASE_URL + '/api'
 const url = 'http://' + test + '/api'
 
 console.log("url= ", url)
 console.log("test= ", test)
-console.log("env= ", process.env.BASE_URL)
+console.log("env= ", process.env.REACT_APP_BASE_URL)
 
 let api = axios.create({
 //   baseURL: 'http://localhost/api', 
